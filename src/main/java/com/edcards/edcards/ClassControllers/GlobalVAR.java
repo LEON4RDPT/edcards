65950e1ade7724e4f9ed27c0eb9a7ede7269ce41
Leoo <avramadriana3@gmail.com>
2024-05-31 11:28:11 +0100

    Update 1.1

--- src/main/java/com/edcards/edcards/ClassControllers/GlobalVAR.java
+++ src/main/java/com/edcards/edcards/ClassControllers/GlobalVAR.java
@@ -31,7 +31,6 @@ public class    GlobalVAR {
 
         public static Stage currentStage;
 
-
         public static void setPessoaAtual(Pessoa pessoa) {
             pessoaAtual = pessoa;
         }
