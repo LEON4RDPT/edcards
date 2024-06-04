b8fbfc05767f50e12ec235870261bfc8c07c0fdc
guilh <guilh@GUIPC>
2024-06-04 22:41:36 +0100

    1.4

---
+++ src/main/java/com/edcards/edcards/FormControllers/POSAdmin.java
@@ -0,0 +1,39 @@
+package com.edcards.edcards.FormControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class POSAdmin {
+    @FXML
+    private Button exit;
+    @FXML
+    private Button addUsr;
+    @FXML
+    private Button modUsr;
+    @FXML
+    private Button remUsr;
+    @FXML
+    private Button addCard;
+    @FXML
+    private Button modCard;
+    @FXML
+    private Button modPinUsr;
+    @FXML
+    private Button addHorario;
+    @FXML
+    private Button modHorario;
+    @FXML
+    private Button remHorario;
+    @FXML
+    private Button addPrdt;
+    @FXML
+    private Button modPrdt;
+    @FXML
+    private Button remPrdt;
+    @FXML
+    private Button viewTransacs;
+
+
+
+
+}
---
+++ src/main/java/com/edcards/edcards/tests/enumTest.java
@@ -0,0 +1,11 @@
+package com.edcards.edcards.tests;
+
+import com.edcards.edcards.Programa.Controllers.Enums.ConfEnum;
+import com.edcards.edcards.Programa.Controllers.Enums.ErrorEnum;
+
+public class enumTest {
+    public static void main(String[] args) {
+        System.out.println(ConfEnum.conf0.toString());
+        System.out.println(ErrorEnum.err0.toString());
+    }
+}
