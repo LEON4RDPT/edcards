8b6238a7b04307cd30cf630c695ac16452332743
Leoo <avramadriana3@gmail.com>
2024-06-05 16:04:21 +0100

    Update 1.2.2.8 (POS + CSS + FeedBack)

--- src/main/java/com/edcards/edcards/Programa/Controllers/FeedBackController.java
+++ src/main/java/com/edcards/edcards/Programa/Controllers/FeedBackController.java
@@ -2,12 +2,25 @@ package com.edcards.edcards.Programa.Controllers;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.util.Optional;
 
 public class FeedBackController {
     private static Stage currentStage = GlobalVAR.Dados.getCurrentStage();
+
+    private Image imageQuestion = new Image(getClass().getResourceAsStream("/com/edcards/edcards/images/icons/question.png"));
+
+
+    private void setPic(Image img, Alert alert) {
+        ImageView imageView = new ImageView(img);
+
+        alert.setGraphic(imageView);
+        //todo
+    }
+
 
 
     public static void feedbackErro(String message) {
@@ -24,13 +37,13 @@ public class FeedBackController {
 
     public static boolean feedbackYesNo(String message) {
         currentStage = GlobalVAR.Dados.getCurrentStage();
-
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmação");
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.initOwner(currentStage);
         alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+
         Optional<ButtonType> result = alert.showAndWait();
         return result.isPresent() && result.get() == ButtonType.YES;
     }
