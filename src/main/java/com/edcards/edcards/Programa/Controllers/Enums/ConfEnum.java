f3f3883e8a792f67fbdf34588e6e53c8967f2a24
PT <PT@GHOSTSPECTRE-GU>
2024-06-05 10:15:45 +0100

    mudança de pc

--- src/main/java/com/edcards/edcards/Programa/Controllers/Enums/ConfEnum.java
+++ src/main/java/com/edcards/edcards/Programa/Controllers/Enums/ConfEnum.java
@@ -3,24 +3,86 @@ package com.edcards.edcards.Programa.Controllers.Enums;
 public enum ConfEnum {
     conf0 {
         public String toString() {
-            return "Utilizador Registado com sucesso";
+            return "Utilizador registado com sucesso";
         }
     },
-    conf1 {
+    conf1{
         public String toString() {
-            return "Compra Realizada e Registada com sucesso";
+            return "Utilizador alterado com sucesso";
         }
     },
     conf2 {
         public String toString() {
-            return "Refeição Marcada com sucesso";
+            return "Utilizador apagado com sucesso";
         }
     },
     conf3 {
         public String toString() {
-            return "Utilizador Apagado com sucesso";
+            return "PIN alterado com sucesso";
         }
     },
     conf4{
-        public String toString() { return "Saldo Adicionado com sucesso";}}
+        public String toString() {
+            return "Horario adicionado com Sucesso";}
+    },
+    conf5{
+        public String toString() {
+            return "Horario alterado com Sucesso";}
+    },
+    conf6{
+        public String toString() {
+            return "Horario removido com Sucesso";}
+    },
+    conf7{
+        public String toString() {
+            return "Cartão Adicionado com Sucesso";}
+    },
+    conf8{
+        public String toString() {
+            return "Cartão do utilizador alterado com Sucesso";}
+    },
+    conf9{
+        public String toString() {
+            return "";}
+    },
+    conf10{
+        public String toString() {
+            return "Produto Adicionado com Sucesso";}
+    },
+    conf11{
+        public String toString() {
+            return "Produto Alterado com Sucesso";}
+    },
+    conf12{
+        public String toString() {
+            return "Produto Removido com Sucesso";}
+    }
+    ,conf13{
+        public String toString() {
+            return "Saldo adicionado com sucesso";}
+    },
+    conf97 {
+        public String toString() {
+            return "Compra realizada e registada com sucesso";
+        }
+    },
+
+    conf99{
+        public String toString() {
+            return "Refeição marcada com sucesso";}
+    };
+
+    public int toDbValue() {return this.ordinal();}
+    public static ErrorEnum fromDbValue(int value) {
+        return ErrorEnum.values()[value];
+    }
+
+    public static String[] getStringValues() {
+        ErrorEnum[] values = ErrorEnum.values();
+        String[] stringValues = new String[values.length];
+        for (int i = 0; i < values.length; i++) {
+            stringValues[i] = values[i].toString();
+        }
+        return stringValues;
+    }
 }
