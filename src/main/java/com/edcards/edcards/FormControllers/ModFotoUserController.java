379813cde5e340887fdeb7f5df76a24f57e8eb3e
guilh <guilh@GUIPC>
2024-06-10 19:00:15 +0100

    mud casa

---
+++ src/main/java/com/edcards/edcards/FormControllers/ModFotoUserController.java
@@ -0,0 +1,68 @@
+package com.edcards.edcards.FormControllers;
+
+import com.edcards.edcards.DataTable.CartaoBLL;
+import com.edcards.edcards.DataTable.UsersBLL;
+import com.edcards.edcards.Programa.Classes.Pessoa;
+import com.edcards.edcards.Programa.Controllers.LerCartao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ModFotoUserController {
+    private volatile boolean isRunning = true;
+    private ExecutorService nfcExecutar = Executors.newSingleThreadExecutor();
+    @FXML
+    private ImageView imageUser;
+    @FXML
+    private Text nameText;
+    @FXML
+    private ComboBox<String> userPicker;
+    @FXML
+    private Button changePic, backBtn;
+
+    public void initialize(){
+        aguardarCartao();
+
+        imageUser.setOnMouseClicked(event -> {
+            FileChooser selFoto = new FileChooser();
+            selFoto.setTitle("Escolha uma Foto...");
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Imagens (*.png, *.jpg)", "*.png", "*.jpg");
+            selFoto.getExtensionFilters().add(extFilter);
+
+            File foto = selFoto.showOpenDialog(null);
+
+            if (foto != null) {
+                Image image = new Image(foto.toURI().toString());
+                imageUser.setImage(image);
+            }
+        });
+    }
+
+    private void aguardarCartao() {
+        nfcExecutar.submit(() -> {
+            while (isRunning) {
+                try {
+                    String idCartao = LerCartao.lerIDCartao();
+                    int user = CartaoBLL.getIdUserByNFC(idCartao);
+                    UsersBLL.getUser(user);
+                    break;
+
+                } catch (Exception ignored) {
+                }
+            }
+        });
+    }
+}
