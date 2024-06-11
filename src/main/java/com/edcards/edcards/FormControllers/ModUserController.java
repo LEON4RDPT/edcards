233e35b77ae471b95d805ac605e42f4502ed7075
Leoo <avramadriana3@gmail.com>
2024-06-11 10:59:42 +0100

    Update 1.2.2.9 (FIX)

--- src/main/java/com/edcards/edcards/FormControllers/ModUserController.java
+++ src/main/java/com/edcards/edcards/FormControllers/ModUserController.java
@@ -6,14 +6,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
