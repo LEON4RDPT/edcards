2217a286107e0efabf5fcbc7ee6c427d49f4d76e
Leoo <avramadriana3@gmail.com>
2024-07-08 02:02:20 +0100

    leoo fix (Refeicao FIX)

--- src/main/java/com/edcards/edcards/DataTable/RefeicaoBLL.java
+++ src/main/java/com/edcards/edcards/DataTable/RefeicaoBLL.java
@@ -1,7 +1,21 @@
 package com.edcards.edcards.DataTable;
 
 import com.edcards.edcards.DataTable.Settings.DAL;
+import com.edcards.edcards.DataTable.Settings.DefaultBLL;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RefeicaoBLL extends DAL {
     public RefeicaoBLL() { super("refeicao"); }
+
+    public static void inserirRefeicao(int produto_id, Date dataRefeicao) {
+        DefaultBLL bll = new DefaultBLL("refeicao");
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("produto_id", produto_id);
+        params.put("data", dataRefeicao);
+        bll.insert(params);
+    }
+
 }
