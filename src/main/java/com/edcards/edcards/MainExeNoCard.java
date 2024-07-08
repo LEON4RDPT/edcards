ef436c9d747366ff7f4395151f688575441774a1
Leoo <avramadriana3@gmail.com>
2024-07-08 02:46:36 +0100

    leoo fix (Horario Form Done!)

--- src/main/java/com/edcards/edcards/MainExeNoCard.java
+++ src/main/java/com/edcards/edcards/MainExeNoCard.java
@@ -10,9 +10,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainExeNoCard extends Application {
+    private int id = 2;
     @Override
     public void start(Stage stage) throws IOException {
-        GlobalVAR.Dados.setPessoaAtual(UsersBLL.getUser(2));
+        GlobalVAR.Dados.setPessoaAtual(UsersBLL.getUser(id));
         FXMLLoader fxmlLoader = new FXMLLoader(MainExe.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Aplicação");
