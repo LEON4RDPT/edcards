45af83994da7102bac6efa57ff1142167fcac5cc
Leoo <avramadriana3@gmail.com>
2024-07-08 01:56:21 +0100

    leoo fix (Refeicao FIX)

---
+++ src/main/java/com/edcards/edcards/DataTable/RefeicaoBLL.java
@@ -0,0 +1,7 @@
+package com.edcards.edcards.DataTable;
+
+import com.edcards.edcards.DataTable.Settings.DAL;
+
+public class RefeicaoBLL extends DAL {
+    public RefeicaoBLL() { super("refeicao"); }
+}
---
+++ src/main/java/com/edcards/edcards/DataTable/RefeicaoMarcadaBLL.java
@@ -0,0 +1,13 @@
+package com.edcards.edcards.DataTable;
+
+import com.edcards.edcards.DataTable.Settings.DAL;
+
+public class RefeicaoMarcadaBLL extends DAL {
+    public RefeicaoMarcadaBLL() { super("refeicao_marcada");}
+
+
+
+
+
+
+}
