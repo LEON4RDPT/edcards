a0c4b60b58f57e3871e5680e60e31f8ec4657436
Leoo <avramadriana3@gmail.com>
2024-07-08 20:58:46 +0100

    leoo (Refeicoes)

---
+++ src/main/java/com/edcards/edcards/FormControllers/AddRef.java
@@ -0,0 +1,15 @@
+package com.edcards.edcards.FormControllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class AddRef {
+    public AnchorPane leftPane;
+    public AnchorPane rightPane;
+    public Button exit;
+
+    public void handleExit(ActionEvent actionEvent) {
+
+    }
+}
