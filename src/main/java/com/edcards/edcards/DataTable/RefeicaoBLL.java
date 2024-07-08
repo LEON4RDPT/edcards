b2eb861dc0105ae03765d95bdbf9cf7923b4a1bd
Leoo <avramadriana3@gmail.com>
2024-07-08 02:17:51 +0100

    leoo fix (Refeicao FIX)

--- src/main/java/com/edcards/edcards/DataTable/RefeicaoBLL.java
+++ src/main/java/com/edcards/edcards/DataTable/RefeicaoBLL.java
@@ -10,11 +10,40 @@ import java.util.Map;
 public class RefeicaoBLL extends DAL {
     public RefeicaoBLL() { super("refeicao"); }
 
+    public static boolean existeRefeicao(int id) {
+        return new DefaultBLL("refeicao").hasRows("id_ref", id);
+    }
+
+    public static boolean existeRefeicao(Date data) {
+        return new DefaultBLL("refeicao").hasRows("data", data);
+    }
+
+    //id_marc != id_ref //IMPORTANT!
+    public static boolean marcouRefeicao(int idUser, int idRefMarc) {
+        return new DefaultBLL("refeicao_marcada").hasObject("usuario_id",idUser,"id_marc",idRefMarc);
+
+    }
+
+
     public static void inserirRefeicao(int produto_id, Date dataRefeicao) {
         DefaultBLL bll = new DefaultBLL("refeicao");
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("produto_id", produto_id);
         params.put("data", dataRefeicao);
+        bll.insert(params);
+    }
+
+
+    public static void marcarRefeicao(int idRefeicao, int idUsuario) {
+        if (!existeRefeicao(idRefeicao)) {
+            //feedback
+            return;
+        }
+
+        DefaultBLL bll = new DefaultBLL("refeicao_marcada");
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("refeicao_id", idRefeicao);
+        params.put("usuario_id", idUsuario);
         bll.insert(params);
     }
 
